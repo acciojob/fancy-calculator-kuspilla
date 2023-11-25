d402ea533a0541873c13c93a1e0b813cc3751f0f
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-11-25 07:14:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let a= prompt("Enter first val");
+
+let b = prompt("Enter second val");
+
+a = parseInt(a);
+b = parseInt(b);
+   a = a * 2;
+   let ans = a + b;
+
+ alert(`The final result is: ${ans}`);
